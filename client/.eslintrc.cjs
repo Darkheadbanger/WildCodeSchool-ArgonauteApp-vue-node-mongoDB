e4ab13ccba4e8eb50cbd9dab2b037a54f636f07d
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-12-02 16:54:43 +0100

    problem prettier "error rn" solved

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -5,10 +5,22 @@ module.exports = {
   root: true,
   extends: [
     "plugin:vue/vue3-essential",
+    "plugin:prettier/recommended",
+    "plugin:node/recommended",
+    "@vue/prettier",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
   ],
+  rules: {
+    // "prettier/prettier": ["error", { endOfLine: "off" }],
+    "prettier/prettier": ["error"],
+  },
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  env: {
+    node: true,
   },
 };
