8f7c81862cc97d39ca7d840d1dae18f518691d0d
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-12-03 14:06:14 +0100

    header done

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,46 @@
 <script setup>
-// import { RouterLink, RouterView } from 'vue-router'
+import Header from "@/components/TheHeader.vue";
+// import Main from "@/components/Main.vue";
+// import Footer from "@/components/Footer.vue";
 </script>
+
 <template>
-  <h1 class="h1">Hello</h1>
+  <div class="cnotainer">
+    <header>
+      <Header title=" Les Argonautes" />
+    </header>
+  </div>
 </template>
 
 <style lang="scss">
-.h1 {
-  color: $color1;
+/* Button add */
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+*,
+*::after,
+*::before {
+  margin: 0 auto;
+  padding: 0 auto;
+  box-sizing: inherit;
 }
+
+header {
+  background: #f4f4f4;
+  text-align: center;
+  padding: 2em;
+}
+
+.cnotainer {
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  text-align: center;
+}
+/* Jusqu'ici */
 </style>
--- client/src/components/TheHeader.vue
+++ client/src/components/TheHeader.vue
@@ -1,4 +1,31 @@
-<script setup></script>
+<script setup>
+import logoWCS from "../assets/Wild-Code-School-logo.png";
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
 
-<template></template>
-<style lang="scss" scoped></style>
+<template>
+  <h1>
+    <img :src="logoWCS" alt="Logo du wild code school" />
+    {{ props.title }}
+  </h1>
+</template>
+
+<style lang="scss" scoped>
+header {
+  h1 {
+    font-size: 2.5em;
+    font-weight: bold;
+    text-align: center;
+    img {
+      width: 17%;
+      min-width: 96px;
+      vertical-align: middle;
+    }
+  }
+}
+</style>
