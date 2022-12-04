76609956ac26adf73bb7eeaa69a6f2bdc9d671a1
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-12-04 10:22:31 +0100

    member created

--- client/src/components/TheMain.vue
+++ client/src/components/TheMain.vue
@@ -1,15 +1,27 @@
 <script setup>
 import Form from "./TheForm.vue";
+import Member from "./TheMember.vue";
 </script>
 
 <template>
   <main>
-    <section class="containerForm containerForm__element containerForm__element--modifier">
-      <h2>Add  an Argonautes</h2>
+    <section
+      class="containerForm containerForm__element containerForm__element--modifier"
+    >
+      <h2>Add an Argonautes</h2>
       <Form />
     </section>
-    <section class="containerMember containerMember__element containerMember__element--modifier">
+    <section
+      class="containerMember containerMember__element containerMember__element--modifier"
+    >
       <h3>Membres de l'équipage</h3>
+      <ul class="member-list-container member-list-container__element">
+        <Member
+          v-for="crewMember in crewArray"
+          :key="crewMember.id"
+          :member="crewMember"
+        />
+      </ul>
     </section>
   </main>
 </template>
@@ -18,7 +30,7 @@ main {
   h2 {
     font-size: 1.5em;
   }
-  .containerMember{
+  .containerMember {
     background: $member-background;
   }
 }
