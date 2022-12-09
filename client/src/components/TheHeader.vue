b81d4ca58f5d3693efc494dcb2a9b49c70bb73c1
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-12-09 15:34:40 +0100

    breakpoints created

--- client/src/components/TheHeader.vue
+++ client/src/components/TheHeader.vue
@@ -12,7 +12,7 @@ const props = defineProps({
   <header>
     <h1>
       <img :src="logoWCS" alt="Logo du wild code school" />
-      {{ props.title }}
+      <p>{{ props.title }}</p>
     </h1>
   </header>
 </template>
@@ -23,9 +23,12 @@ header {
   text-align: center;
   padding: 2em;
   h1 {
+    display: block ruby;
     @include header-title;
+    
     img {
       @include header-logo;
+      padding: 0 2rem 0 0;
     }
   }
 }
